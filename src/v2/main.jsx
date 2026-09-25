d31d2d5cf929d921d8b668b34b0b3f12e082bae7
torvotools <torvotools@gmail.com>
2026-09-25 18:32:30 +0530

    Remove retired duplicate public UI layer atomically

--- src/v2/main.jsx
+++ src/v2/main.jsx
@@ -18,7 +18,6 @@ import './dealer-addon-ui.css';
 import './dealer-rewards-ui.css';
 import './product-discovery.css';
 import './login-ui.css';
-import './public-mobile-fix.css';
 import './public-product-showcase.css';
 import './smart-search-ui.css';
 import './public-catalog-browser.css';
