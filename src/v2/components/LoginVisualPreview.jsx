9129120c7ef9696d3a80fa10098cc7099af00ca4
torvotools <torvotools@gmail.com>
2026-09-26 20:49:21 +0530

    Isolate Business Login user field rendering

--- src/v2/components/LoginVisualPreview.jsx
+++ src/v2/components/LoginVisualPreview.jsx
@@ -159,9 +159,9 @@ export default function LoginVisualPreview() {
 
           <label className="businessFinalLabel">
             USER ID
-            <div className={`businessFinalInput ${userValid ? 'isValid' : (userInvalid ? 'isInvalid' : '')}`}>
+            <div className={`businessFinalInput businessFinalUserClean ${userValid ? 'isValid' : (userInvalid ? 'isInvalid' : '')}`}>
               <UserRound />
-              <input className="businessFinalNativeInput"
+              <input className="businessFinalNativeInput businessFinalUserCleanInput"
                 aria-label="USER ID"
                 autoFocus
                 value={username}
